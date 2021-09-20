e7d0fb81156eab57ca553aab3e7a5462390967b8
sanchezd90 <sanchezd90@gmail.com>
2021-09-20 11:09:23 -0300

    Initial commit

---
+++ Shared/Entity/Paisaje.cs
@@ -0,0 +1,12 @@
+using System;
+namespace blazorProyI.Shared.Entity
+{
+    public class Landscape
+    {
+        public string LandscapeType { get; set; }
+        public string LandscapeColor { get; set; }
+        public string LandscapeMood { get; set; }
+        public string LandscapeImage { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
